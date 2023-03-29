2d80a9ef5a6659839380fa156b07bed06b41331a
Supreeth <psaisupreeth10111997@gmail.com>
2023-03-29 16:20:55 -0500

    bonus

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
-
+let more=0
+let sum=0
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  more=more+1
+  sum=sum+more
+  res.send('Sum is :'+sum);
 });
 
 module.exports = router;
